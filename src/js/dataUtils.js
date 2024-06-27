fc2126a06f0bd92c894dbe4981f4885dc6c8ebb5
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-06-27 12:29:35 +0300

    commit

--- src/js/dataUtils.js
+++ src/js/dataUtils.js
@@ -1,8 +1,14 @@
 import * as fs from "fs";
 
 const messagesPath = "./data/messages.json";
+const dirs = [
+  "./data",
+  "./data/audio",
+  "./data/files",
+  "./data/video"]
 
 export function loadMessages() {
+  initDirs();
   return (
     fs.existsSync(messagesPath) &&
     JSON.parse(fs.readFileSync(messagesPath).toString())
@@ -10,10 +16,12 @@ export function loadMessages() {
 }
 
 export function saveMessages(json) {
+  initDirs();
   fs.writeFileSync(messagesPath, JSON.stringify(json));
 }
 
 export function saveAttachments(attachments) {
+  initDirs();
   for (const attachment of attachments) {
     const dirPath = `./data/files/${attachment.messageId}`;
     fs.mkdirSync(dirPath);
@@ -24,4 +32,12 @@ export function saveAttachments(attachments) {
 
 export function getAttachmentPath(messageId, name) {
   return `./data/files/${messageId}/${name}`;
+}
+
+export function initDirs() {
+  for (const dir of dirs) {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+  }
 }
